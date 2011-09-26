e3ab369a513f106ae1bee8d84eecd8a4d730d3b6
manmoth <janvidar33@gmail.com>
2011-09-26 15:36:11 +0200

    mmm

--- StructAlgoSim/sim/functions/Push.java
+++ StructAlgoSim/sim/functions/Push.java
@@ -8,15 +8,7 @@ import sim.gui.elements.Variable;
 import sim.structures.Stack;
 
 public class Push extends Function {
-<<<<<<< HEAD
 	public Push(Stack s, Variable v){
-=======
-<<<<<<< HEAD
-	Push(Stack s, Variable v){
-=======
-	public Push(Stack s, Variable v){
->>>>>>> origin/master
->>>>>>> b35b16214d42b0d7fdf72e6c8907fd0766841590
 		super("Push");
 	}
 	@Override
