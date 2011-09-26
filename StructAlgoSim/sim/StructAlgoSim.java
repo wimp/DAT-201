5c2787bf5d91f0b508ccd73878515225ccbdb12d
Rune Borge Kalleberg <rune@kallebergmedia.no>
2011-09-26 13:38:47 +0200

    First commit

---
+++ StructAlgoSim/sim/StructAlgoSim.java
@@ -0,0 +1,20 @@
+package sim;
+
+import javax.swing.JFrame;
+
+public class StructAlgoSim {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setTitle("StructAlgoSim 0.1");
+		frame.setSize(700,600);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		  
+	}
+
+}
