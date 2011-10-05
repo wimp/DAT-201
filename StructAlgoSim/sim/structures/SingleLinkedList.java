e4ab0760a20d974e90908cf41c7ca13ca45767ac
Rune Lien <rune@wimponline.net>
2011-10-05 15:47:39 +0200

    More methods added to the SingleLinkedList class. Need addElementAtBeginning() and addElementAtEnd() methods as well as getNextElement() and getPreviousElement()

--- StructAlgoSim/sim/structures/SingleLinkedList.java
+++ StructAlgoSim/sim/structures/SingleLinkedList.java
@@ -21,17 +21,45 @@ public class SingleLinkedList {
 		v.add(null);
 	}
 	
-	public void insertBeforeElement(Node element){
-		element.getPrevious();
-//		Node n = new Node(v.)
+	public void insertBeforeElement(Node element, Object value){
+		Node n = new Node(v.indexOf(element),element.getPrevious(),element, value);
+		element.getPrevious().setNext(n);
+		v.insertElementAt(n, v.indexOf(element));
+		for(int i = 0;i<v.size();i++){
+			Node el = v.get(i);
+			if(el != null)
+				el.setIndex(v.indexOf(el));
+		}
+		element.setPrevious(n);
 	}
 	
+	public void insertAfterElement(Node element, Object value){
+		Node n = new Node(v.indexOf(element),element,element.getNext(), value);
+		element.getNext().setPrevious(n);
+		v.insertElementAt(n, element.getNext().getIndex());
+		
+		for(int i = 0;i<v.size();i++){
+			Node el = v.get(i);
+			if(el != null)
+				el.setIndex(v.indexOf(el));
+		}
+		
+		element.setNext(n);
+	}
+	
+	/**
+	 * The Node class is an inner class of {@link SingleLinkedList} that contains information about where an 
+	 * element is in relationship to other elements in a list
+	 *
+	 */
 	public class Node{
-		private Object value;
-		private int index;
-		private Node next;
-		private Node previous;
-
+	// Class variables
+		private Object 	value;
+		private int 	index;
+		private Node 	next;
+		private Node 	previous;
+	
+	// Getters and setters
 		public Node getNext() {
 			return next;
 		}
@@ -48,8 +76,28 @@ public class SingleLinkedList {
 			this.previous = previous;
 		}
 		
-		public Node(int index, Node previous, Node next){
-			
+		public int getIndex(){
+			return index;
+		}
+		
+		public void setIndex(int index){
+			this.index = index;
+		}
+		
+		public Object getValue() {
+			return value;
+		}
+
+		public void setValue(Object value) {
+			this.value = value;
+		}
+	
+	// Class constructor
+		public Node(int index, Node previous, Node next, Object value){
+			this.index 		= index;
+			this.previous 	= previous;
+			this.next 		= next;
+			this.value		= value;
 		}
 	}
 }
