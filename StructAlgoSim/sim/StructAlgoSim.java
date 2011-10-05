4ddd70cdb0c009f50c91e0e3d9d477b7084cce07
manmoth <janvidar33@gmail.com>
2011-10-05 15:13:09 +0200

    fix

--- StructAlgoSim/sim/StructAlgoSim.java
+++ StructAlgoSim/sim/StructAlgoSim.java
@@ -1,13 +1,8 @@
 package sim;
 
-import java.awt.Rectangle;
-
 import javax.swing.JFrame;
 
-import sim.functions.Pop;
-import sim.functions.Push;
-import sim.structures.Stack;
-import sim.structures.Variable;
+import sim.demos.RailwayPostfix;
 
 public class StructAlgoSim {
 
@@ -15,22 +10,23 @@ public class StructAlgoSim {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setTitle("StructAlgoSim 0.1");
-		frame.setSize(700,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-
-		Stack s = new Stack(new Rectangle(100,300, 100, 100));
-		Variable v = new Variable(new Rectangle(50,10,100,20),"win", true);
-		Pop pop = new Pop(new Rectangle(100,200,100,25),s, v);
-		Push push = new Push(new Rectangle(200,200,100,25), v, s);
-		frame.add(pop.getGuiElement());
-		frame.add(push.getGuiElement());
-		frame.add(v.getGuiElement());
-		frame.add(s.getGuiElement());
-		frame.validate();
-		frame.setVisible(true);
+		RailwayPostfix rsim = new RailwayPostfix();
+//		JFrame frame = new JFrame();
+//		frame.setTitle("StructAlgoSim 0.1");
+//		frame.setSize(700,600);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setLayout(null);
+//
+//		Stack s = new Stack(new Rectangle(100,300, 100, 100));
+//		Variable v = new Variable(new Rectangle(50,10,100,20),"win", true);
+//		Pop pop = new Pop(new Rectangle(100,200,100,25),s, v);
+//		Push push = new Push(new Rectangle(200,200,100,25), v, s);
+//		frame.add(pop.getGuiElement());
+//		frame.add(push.getGuiElement());
+//		frame.add(v.getGuiElement());
+//		frame.add(s.getGuiElement());
+//		frame.validate();
+//		frame.setVisible(true);
 	}
 
 }
