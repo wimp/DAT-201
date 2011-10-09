9ac5392ba40629037a1cc9aeae83ba79b2921b88
unknown <Jan-Vidar@.(none)>
2011-10-09 02:43:43 +0200

    major changes to the linked list class

---
+++ StructAlgoSim/sim/functions/Add.java
@@ -0,0 +1,47 @@
+package sim.functions;
+
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import sim.gui.elements.GuiElement;
+import sim.gui.elements.GuiFunction;
+import sim.structures.LinkedList;
+import sim.structures.Stack;
+import sim.structures.Variable;
+
+/**
+ * Pop - Instances of this class is used with structures such as stack to fetch the top element. (LIFO)
+ * @author 
+ */
+public class Add implements ActionListener {
+// Class variables //
+	LinkedList l;
+	Variable v;
+	GuiFunction gui;
+	
+// Getters and setters //
+	public GuiElement getGuiElement(){
+		return gui;
+	}
+	
+// Class constructor //
+	/**
+	 * The class constructor. Initializes the graphical element - {@link GuiFunction}
+	 * @param bounds 	- The size and placement of the graphical element
+	 * @param l 		- The structure on which to perform the add()-action
+	 * @param v 		- The output variable
+	 */
+	public Add(Rectangle bounds, LinkedList l, Variable v) {
+		gui = new GuiFunction(bounds,"Add");
+		gui.getButton().addActionListener(this);
+		this.l=l;
+		this.v=v;
+	}
+
+// Action Listener implementation //
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		l.addFirst(v.getValue());
+	}
+}
