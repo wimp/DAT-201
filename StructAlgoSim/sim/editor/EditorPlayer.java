9d1714c5e1e6e993cff5380eea0a1e63ae10148b
Jan-Vidar Oelberg <janvidar33@gmail.com>
2011-10-25 11:20:23 +0200

    lose...

---
+++ StructAlgoSim/sim/editor/EditorPlayer.java
@@ -0,0 +1,12 @@
+package sim.editor;
+
+import javax.swing.JPanel;
+
+public class EditorPlayer{
+	JPanel editorPanel;
+	
+	public EditorPlayer(JPanel editorPanel){
+		this.editorPanel = editorPanel;
+		
+	}
+}
