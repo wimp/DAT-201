fdb4642197ce3178e7e7020feb98fc8fa7dafad9
Rune Borge Kalleberg <rune@kallebergmedia.no>
2011-10-31 11:50:26 +0100

    Added demos

---
+++ StructAlgoSim/sim/demos/ArithmeticTree.java
@@ -0,0 +1,30 @@
+package sim.demos;
+
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+import sim.structures.Tree;
+
+public class ArithmeticTree {
+	public static void main(String[] args) {
+		
+		new ArithmeticTree();
+		
+	}
+	
+	public ArithmeticTree(){
+	JFrame frame = new JFrame();
+	frame.setTitle("StructAlgoSim 0.1 - Arithmetic tree");
+	frame.setSize(800,550);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setLayout(null);
+	
+	Tree tree = new Tree(new Rectangle(200,200,200,200), true);
+	String infix = "2+2*2";
+	String postfix = "2 2 2 +*";
+	
+	frame.validate();
+	frame.setVisible(true);
+}
+}
