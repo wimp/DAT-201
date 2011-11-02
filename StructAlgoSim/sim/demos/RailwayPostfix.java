f07f189833bf6b88b156a412491bf7dbeb7c7f24
Rune Borge Kalleberg <rune@kallebergmedia.no>
2011-11-02 14:08:28 +0100

    Added Bubblesort algorithm demo with animation

--- StructAlgoSim/sim/demos/RailwayPostfix.java
+++ StructAlgoSim/sim/demos/RailwayPostfix.java
@@ -2,21 +2,20 @@ package sim.demos;
 
 import java.awt.Rectangle;
 
-import javax.swing.JFrame;
-
+import sim.functions.Get;
 import sim.functions.Pop;
 import sim.functions.Push;
 import sim.structures.Stack;
 import sim.structures.Variable;
 
 public class RailwayPostfix {
-/*	public RailwayPostfix(){
-		JFrame frame = new JFrame();
-		frame.setTitle("StructAlgoSim 0.1 - infix to postfix using railway algorithm");
-		frame.setSize(700,600);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(null);
-		
+	public static void main(String[] args) {
+
+		new RailwayPostfix();
+
+	}
+	public RailwayPostfix(){
+		DemoFrame frame = new DemoFrame("infix to postfix using railway algorith");		
 		
 		Stack s = new Stack(new Rectangle(325,300, 70, 200));
 		Variable si = new Variable(new Rectangle(390,170,60,50),"#",false);
@@ -24,31 +23,40 @@ public class RailwayPostfix {
 
 		Variable infix = new Variable(new Rectangle(500,170,200,50),"a+b*c-e",true);
 		
-		MoveChar moveInfix = new MoveChar(new Rectangle(450,170,50,50),Direction.LEFT,infix,si);
-		MoveChar moveSiSo = new MoveChar(new Rectangle(335,170,55,50),Direction.LEFT,si,so);
+		Get getInfix = new Get(new Rectangle(450,170,50,50),true);
+		getInfix.setSource(infix);
+		getInfix.setTarget(si);
+		
+		Get getSi = new Get(new Rectangle(335,170,55,50),true);
+		getSi.setSource(si);
+		getSi.setTarget(so);
 		
 		Variable postfix = new Variable(new Rectangle(25,170,200,50),"                               ",true);
-		MoveChar movePostfix = new MoveChar(new Rectangle(225,170,50,50),Direction.LEFT,so,postfix);
+		
+		Get movePostfix = new Get(new Rectangle(225,170,50,50),true);
+		movePostfix.setSource(so);
+		movePostfix.setTarget(postfix);
 		
 		Pop popSi = new Pop(new Rectangle(405,300,70,25),s, si);
 		Push pushSi = new Push(new Rectangle(405,350,70,25),si, s);
 		Pop popSo = new Pop(new Rectangle(225,300,70,25),s, so);
 		Push pushSo = new Push(new Rectangle(225,350,70,25),so, s);
 		
-		frame.add(popSi.			getGuiElement());
-		frame.add(pushSi.			getGuiElement());
-		frame.add(popSo.			getGuiElement());
-		frame.add(pushSo.			getGuiElement());
-		frame.add(moveSiSo.         getGuiElement());
-		frame.add(si.			getGuiElement());
-		frame.add(so.			getGuiElement());
-		frame.add(s.			getGuiElement());
-		frame.add(infix.		getGuiElement());
-		frame.add(postfix.		getGuiElement());
-		frame.add(moveInfix.	getGuiElement());
-		frame.add(movePostfix.	getGuiElement());
+		frame.add(popSi);
+		frame.add(pushSi);
+		frame.add(popSo);
+		frame.add(pushSo);
+		frame.add(si);
+		frame.add(so);
+		frame.add(s);
+		frame.add(infix);
+		frame.add(postfix);
+		frame.add(getInfix);
+		frame.add(getSi);
+		frame.add(movePostfix);
+		
 		frame.validate();
-		frame.setVisible(true);
+//		frame.repaint();
 	}
-	*/
+	
 }
