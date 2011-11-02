0be523bb7a944a526d1883e9c4e3263e89f9a9de
Rune Borge Kalleberg <rune@kallebergmedia.no>
2011-11-02 18:14:48 +0100

    Commented InfoPanel

--- StructAlgoSim/sim/structures/InfoPanel.java
+++ StructAlgoSim/sim/structures/InfoPanel.java
@@ -4,31 +4,72 @@ import java.awt.Rectangle;
 
 import sim.gui.elements.GuiInfo;
 
+/**
+ * Informationpanel using a JTextArea. Useful for displaying demo-usage info and various algorithm explanations
+ *
+ */
 public class InfoPanel {
 	GuiInfo gui;
 	
+	/**
+	 * Returns the GuiElement component for adding to JFrame/JPanel
+	 * @return
+	 */
 	public GuiInfo getGuiElement() {
 		return gui;
 	}
+	
+	/**
+	 * Sets the GuiElement
+	 * @param gui
+	 */
 	public void setGuiElement(GuiInfo gui) {
 		this.gui = gui;
 	}
+	
+	/**
+	 * Sets whether or not the textarea is editable or not
+	 * @param editable
+	 */
 	public void setEditable(boolean editable){
 			gui.getTextArea().setEditable(editable);
 	}
+	/**
+	 * Gets wheter or not the textarea is editable or not
+	 * @return Editable boolean
+	 */
 	public boolean getEditable(){
 		return gui.getTextArea().isEditable();
 }
+	/**
+	 * Constructor
+	 * @param bounds - Rectangle containing boundary arguments x,y,width,height
+	 */
 	public InfoPanel(Rectangle bounds){
 		gui = new GuiInfo(bounds, "");
 	}
+	/**
+	 * Gets text within the textarea
+	 * @return String containing all text within the InfoPanel
+	 */
 	public String getValue(){
 		return gui.getTextArea().getText();
 	}
+	/**
+	 * Updates the text contents of the textarea
+	 * @param value
+	 */
 	public void setValue(String value){
 		gui.getTextArea().setText(value);
 		gui.repaint();
 	}
+	
+	/**
+	 * Constructor. Used for programmatic instantiation of the object
+	 * @param bounds
+	 * @param text
+	 * @param editable
+	 */
 	public InfoPanel(Rectangle bounds, String text, boolean editable){
 		gui = new GuiInfo(bounds, text);
 		gui.getTextArea().setEditable(editable);
